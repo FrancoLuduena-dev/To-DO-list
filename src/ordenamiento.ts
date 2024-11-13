19fac41e3f729dd1e0af030419a4c96f3c09ae8b
Nando698 <diazfernando07@gmail.com>
2024-11-12 21:39:02 -0300

    arreglo exports | agrego comentarios typedoc

--- src/ordenamiento.ts
+++ src/ordenamiento.ts
@@ -1,52 +1,52 @@
 import tarea from "./tarea";
 
 class Ordenamiento {
-  // este flag booleano decide si el orden se hace de mayor a menor o menor a mayor
-  // true == menor a mayor
-  // flase == mayor a menor
-  public ordenarPorFecha(
-    listaDesordenada: Array<tarea>,
-    ascendiente: boolean
-  ): Array<tarea> {
-    // ordena la fecha en base a el flag booleano, si es ascendiente o no
-    return listaDesordenada.sort((a, b) => {
-      const fechaA = a.getFechaVencimiento();
-      const fechaB = b.getFechaVencimiento();
+    // este flag booleano decide si el orden se hace de mayor a menor o menor a mayor
+    // true == menor a mayor
+    // flase == mayor a menor
+    public ordenarPorFecha(
+        listaDesordenada: Array<tarea>,
+        ascendiente: boolean
+    ): Array<tarea> {
+        // ordena la fecha en base a el flag booleano, si es ascendiente o no
+        return listaDesordenada.sort((a, b) => {
+            const fechaA = a.getFechaVencimiento();
+            const fechaB = b.getFechaVencimiento();
 
-      // Casos en el que la fecha es Null
-      if (fechaA === null && fechaB === null) return 0;
-      if (fechaA === null) return ascendiente ? 1 : -1;
-      if (fechaB === null) return ascendiente ? -1 : 1;
+            // Casos en el que la fecha es Null
+            if (fechaA === null && fechaB === null) return 0;
+            if (fechaA === null) return ascendiente ? 1 : -1;
+            if (fechaB === null) return ascendiente ? -1 : 1;
 
-      if (ascendiente === true) {
-        if (fechaA.getFullYear() !== fechaB.getFullYear()) {
-          return fechaA.getFullYear() - fechaB.getFullYear();
-        } else if (fechaA.getMonth() !== fechaB.getMonth()) {
-          return fechaA.getMonth() - fechaB.getMonth();
-        } else {
-          return fechaA.getDate() - fechaB.getDate();
-        }
-      } else {
-        if (fechaA.getFullYear() !== fechaB.getFullYear()) {
-          return fechaB.getFullYear() - fechaA.getFullYear();
-        } else if (fechaA.getMonth() !== fechaB.getMonth()) {
-          return fechaB.getMonth() - fechaA.getMonth();
-        } else {
-          return fechaB.getDate() - fechaA.getDate();
-        }
-      }
-    });
-  }
+            if (ascendiente === true) {
+                if (fechaA.getFullYear() !== fechaB.getFullYear()) {
+                    return fechaA.getFullYear() - fechaB.getFullYear();
+                } else if (fechaA.getMonth() !== fechaB.getMonth()) {
+                    return fechaA.getMonth() - fechaB.getMonth();
+                } else {
+                    return fechaA.getDate() - fechaB.getDate();
+                }
+            } else {
+                if (fechaA.getFullYear() !== fechaB.getFullYear()) {
+                    return fechaB.getFullYear() - fechaA.getFullYear();
+                } else if (fechaA.getMonth() !== fechaB.getMonth()) {
+                    return fechaB.getMonth() - fechaA.getMonth();
+                } else {
+                    return fechaB.getDate() - fechaA.getDate();
+                }
+            }
+        });
+    }
 
-  //ordena alfabeticamente
-  public ordenarPorTitulo(listaDesordenada: Array<tarea>): Array<tarea> {
-    listaDesordenada.sort((a, b) => {
-      const tituloA = a.getTitulo();
-      const tituloB = b.getTitulo();
-      return tituloA.localeCompare(tituloB);
-    });
-    return listaDesordenada;
-  }
+    //ordena alfabeticamente
+    public ordenarPorTitulo(listaDesordenada: Array<tarea>): Array<tarea> {
+        listaDesordenada.sort((a, b) => {
+            const tituloA = a.getTitulo();
+            const tituloB = b.getTitulo();
+            return tituloA.localeCompare(tituloB);
+        });
+        return listaDesordenada;
+    }
 
     // ordena por prioridad con el metodo sort
     public ordenarPorPrioridad(
