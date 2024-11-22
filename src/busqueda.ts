03e0b7bb52d4b5a46592cd326de9ea7b060b08a6
FrancoLuduena-dev <franlud53@gmail.com>
2024-11-22 20:27:31 -0300

    agrego typedoc a lo que faltaba

--- src/busqueda.ts
+++ src/busqueda.ts
@@ -1,6 +1,18 @@
 import Tarea from "./tarea";
 
-interface Busqueda{
+/**
+ * Representa la funcionalidad de búsqueda de tareas.
+ * 
+ * @interface Busqueda
+ */
+interface Busqueda {
+    /**
+     * Realiza una búsqueda en un conjunto de tareas basado en un parámetro.
+     * 
+     * @param tarea - Un array de instancias de Tarea.
+     * @param parametro - El criterio de búsqueda. Puede ser de cualquier tipo.
+     * @returns Un array de tareas que coinciden con el criterio de búsqueda.
+     */
     buscar(tarea: Tarea[], parametro: any): Tarea[];
 }
 
